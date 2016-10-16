4ff11662f550abd35316a2957986dede0f04519b
Tomoya Kose <tomoya@mitsuse.jp>
2016-10-16 23:53:17 +0900

    View has its size and position.

--- view.go
+++ view.go
@@ -2,9 +2,25 @@ package cui
 
 /*
 View is a type to define rendering of application's models.
+
+This type stores the attributes required for rendering,
+such as the size and the position of ui component.
 */
 type View struct {
+	Size  Size
+	Point Point
 }
 
 func (view *View) Render() {
+	// TODO: Render `view`.
+}
+
+type Size struct {
+	Width  int
+	Height int
+}
+
+type Point struct {
+	X int
+	Y int
 }
