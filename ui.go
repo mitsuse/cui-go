3e6c01b49492db2e634d7f301aed97752c7c2695
Tomoya Kose <tomoya@mitsuse.jp>
2016-10-16 22:23:37 +0900

    `Renrere` is abstraction of view.

--- ui.go
+++ ui.go
@@ -9,14 +9,18 @@ import (
 
 /*
 Run the main loop of application.
+
+The main loop repeats rendering and event handling.
 */
-func Run() error {
+func Run(renderer Renderer) error {
 	if err := termbox.Init(); err != nil {
 		return err
 	}
 	defer termbox.Close()
 
 	for {
+		render(renderer)
+
 		event := termbox.PollEvent()
 
 		// TODO: Send event to the focused view.
@@ -24,4 +28,17 @@ func Run() error {
 	}
 
 	return nil
+}
+
+func render(renderer Renderer) {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	renderer.Render()
+	termbox.Flush()
+}
+
+/*
+Renderer is abstraction of view.
+*/
+type Renderer interface {
+	Render()
 }
